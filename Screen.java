6d9affa24a55442f8dbe7dc1df868f1e14b791d5
shivazadokar <124267550+shivazadokar@users.noreply.github.com>
2024-07-02 18:18:09 +0530

    Create Screen.java

---
+++ Screen.java
@@ -0,0 +1,28 @@
+
+@Entity
+public class Screen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int seatCapacity;
+    // getters and setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSeatCapacity() {
+		return seatCapacity;
+	}
+	public void setSeatCapacity(int seatCapacity) {
+		this.seatCapacity = seatCapacity;
+	}
+}
