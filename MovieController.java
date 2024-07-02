ca7863e4c12d0930c7c1630f27de80c970b04ec1
shivazadokar <124267550+shivazadokar@users.noreply.github.com>
2024-07-02 18:15:02 +0530

    Create MovieController.java

---
+++ MovieController.java
@@ -0,0 +1,17 @@
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+    @Autowired
+    private MovieService movieService;
+
+    @GetMapping
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Movie movie = movieService.getMovieById(id);
+        return ResponseEntity.ok(movie);
+    }
+}
