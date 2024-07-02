4f4b73c50528281b7618848cf519d3df690192aa
shivazadokar <124267550+shivazadokar@users.noreply.github.com>
2024-07-02 18:17:13 +0530

    Create MovieService.java

---
+++ MovieService.java
@@ -0,0 +1,13 @@
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Movie getMovieById(Long id) {
+        return movieRepository.findById(id).orElse(null);
+    }
+}
